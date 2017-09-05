c1c15a56c8886f04a1e033fc565b14477c78bf89
Deeptesha <Deeptesha.Satpute@Xoriant.Com>
2017-09-05 15:38:27 +0530

    Data Annotation Assignment
    Note:Run as localhost:portno/Student/Index

---
+++ DataAnnotation/Controllers/StudentController.cs
@@ -0,0 +1,26 @@
+﻿using DataAnnotation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DataAnnotation.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Customer
+        [HttpGet]
+        public ActionResult Index ()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Student objstudent)
+        {
+            return View();
+        }
+
+    }
+}
---
+++ DataAnnotation/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DataAnnotation.Models
+{
+    public class Student
+    {
+    
+        [Required]
+        public int StudId { get; set; }
+        [Required]
+        [MinLength(3, ErrorMessage = "Length must be upto 10 chars")]
+        [StringLength(10, ErrorMessage = "Length must be upto 10 chars")]
+       
+        public string StudName { get; set; }
+    }
+}
