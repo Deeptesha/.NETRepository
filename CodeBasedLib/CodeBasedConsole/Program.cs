e29f1904949facf191750df55a521432e47528e5
Deeptesha <Deeptesha.Satpute@Xoriant.Com>
2017-09-05 15:45:09 +0530

    Code First Entity Framework

---
+++ CodeBasedLib/CodeBasedConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeBasedLib;
+
+namespace CodeBasedConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var dept = new DepartmentContext())
+            {
+                Console.WriteLine("Add Department");
+                var deptobj = new Department { Id = 1, Name = "IT", Location = "Pune" };
+                dept.Departments.Add(deptobj);
+                dept.SaveChanges();
+                Console.WriteLine("Record inserted.");
+                
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CodeBasedLib/CodeBasedLib/DepartmentContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBasedLib
+{
+    public class DepartmentContext:DbContext
+    {
+        public DbSet<Department> Departments { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Department>().Property(u=>u.Name).HasColumnName("DeptName");
+        }
+    }
+}
